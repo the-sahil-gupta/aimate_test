d4b733f0a086d4e8acd807ef061da2c31d7d203e
the-sahil-gupta <sahilgtech95@gmail.com>
2024-12-05 16:28:22 +0530

    locomotive scroll  added

--- script.js
+++ script.js
@@ -46,23 +46,3 @@ ScrollTrigger.refresh();
 // 		// end: "top 50%",
 // 	},
 // });
-
-// <!-- ScrollTrigger -->
-// 		<script
-// 			src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.4/ScrollTrigger.min.js"
-// 			integrity="sha512-OzC82YiH3UmMMs6Ydd9f2i7mS+UFL5f977iIoJ6oy07AJT+ePds9QOEtqXztSH29Nzua59fYS36knmMcv79GOg=="
-// 			crossorigin="anonymous"
-// 			referrerpolicy="no-referrer"></script>
-// 		<!-- GSAP -->
-// 		<script
-// 			src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.4/gsap.min.js"
-// 			integrity="sha512-EZI2cBcGPnmR89wTgVnN3602Yyi7muWo8y1B3a8WmIv1J9tYG+udH4LvmYjLiGp37yHB7FfaPBo8ly178m9g4Q=="
-// 			crossorigin="anonymous"
-// 			referrerpolicy="no-referrer"></script>
-// 		<!-- locomotive-scroll script -->
-// 		<script src="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.js"></script>
-
-// <!-- LOCOMOTIVE link -->
-// 		<link
-// rel="stylesheet"
-// href="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.css" />
