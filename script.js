b8afc36d51771783c573d86ec167732f6c6d4d7e
the-sahil-gupta <sahilgtech95@gmail.com>
2024-12-18 01:04:02 +0530

    pricing completed with responsive and working js

--- script.js
+++ script.js
@@ -56,12 +56,31 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 });
 function setToggle(option) {
-	const toggleBtn = document.querySelector('#toggle');
 	const options = document.querySelectorAll('.toggle-options');
+	const individualprice = document.querySelector('#individual-price');
+	const businessprice = document.querySelector('#business-price');
 	options.forEach((opt, index) =>
 		opt.classList.toggle(
 			'active',
 			option === (index === 0 ? 'monthly' : 'yearly')
 		)
+	);
+	option === 'monthly'
+		? ((individualprice.innerText = '$18/m'),
+		  (businessprice.innerText = '$30/m'))
+		: ((individualprice.innerText = '$172.8/y'),
+		  (businessprice.innerText = '$288/y'));
+}
+function setPack(option) {
+	const options = document.querySelectorAll('.price-card');
+
+	// Determine the position of the active background slider
+	let position = 0;
+	if (option === 'starter') position = 0;
+	if (option === 'individual') position = 1;
+	if (option === 'business') position = 2;
+	// Update the slider position
+	options.forEach((opt, index) =>
+		opt.classList.toggle('active', index === position)
 	);
 }
